53f7f872e53248146ffa2d0b8ea5438f7567b2dc
Niket Sahu <nikkysahu8871@gmail.com>
2023-06-08 18:58:55 +0530

    Added check statue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I am Niket Sahu");
         System.out.println("I am Changing somthing");
+        System.out.println("good check status");
     }
 }
